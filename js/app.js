c0a81eca3a6b1028a116c5ed038d983a5b5734d6
Elizabeth Eymann <elizabeth.eymann@gmail.com>
2020-10-09 14:04:41 -0700

    write a basic fetch request using the fetch method to access a dog api.

--- js/app.js
+++ js/app.js
@@ -6,6 +6,9 @@ const form = document.querySelector('form');
 //  FETCH FUNCTIONS
 // ------------------------------------------
 
+fetch('https://dog.ceo/api/breeds/image/random')
+  .then(response => response.json())
+  .then(data => console.log(data.message))
 
 
 // ------------------------------------------
