8987a031a6da811994815c3ae938bac91f29cc5f
Elizabeth Eymann <elizabeth.eymann@gmail.com>
2020-10-09 16:44:05 -0700

    Posting data with fetch. Allow users to write in name and comment in the fields. Info displays in the console.

--- js/app.js
+++ js/app.js
@@ -73,7 +73,26 @@ function fetchBreedImage() {
 
 select.addEventListener('change', fetchBreedImage);
 card.addEventListener('click', fetchBreedImage);
-
+form.addEventListener('submit', postData);
 // ------------------------------------------
 //  POST DATA
 // ------------------------------------------
+
+function postData(e) {
+  e.preventDefault();
+  const name = document.getElementById('name').value;
+  const comment = document.getElementById('comment').value;
+
+  const config = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ name, comment })
+  }
+
+  fetch('https://jsonplaceholder.typicode.com/comments', config)
+  .then(checkStatus)
+  .then(res => res.json())
+  .then(data => console.log(data))
+}
