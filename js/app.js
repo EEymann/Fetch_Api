162e561e42048e5e64609a18784f7ba7bc518a59
Elizabeth Eymann <elizabeth.eymann@gmail.com>
2020-10-09 15:50:10 -0700

    Handle errors with the catch method.

--- js/app.js
+++ js/app.js
@@ -10,11 +10,14 @@ const form = document.querySelector('form');
 
 function fetchData(url) {
   return fetch(url)
-            .then(res => res.json())
+            .then(checkStatus) // If a Fetch promise is fullfilled, first checks the status of response.
+            .then(res => res.json()) //If promise is resolved, it parses the response to JSON.
+            .catch(error => console.log('Looks like there was a problem!', error))
 }
 
 fetchData('https://dog.ceo/api/breeds/list')
 .then(data => generateOptions(data.message))
+
 
 fetchData('https://dog.ceo/api/breeds/image/random')
   .then(data => generateImage(data.message))
@@ -23,6 +26,14 @@ fetchData('https://dog.ceo/api/breeds/image/random')
 // ------------------------------------------
 //  HELPER FUNCTIONS
 // ------------------------------------------
+
+function checkStatus(response) {
+  if(response.ok) {
+    return Promise.resolve(response);
+  } else {
+    return Promise.reject(new Error(response.statusText));
+  }
+}
 
 function generateOptions(data) {
   const options = data.map(item => `
@@ -46,7 +57,7 @@ function fetchBreedImage() {
 
   fetchData(`https://dog.ceo/api/breed/${breed}/images/random`)
     .then(data => {
-      img.src = data.message; 
+      img.src = data.message;
       img.alt = breed;
       p.textContent = `Click to view more ${breed}s`;
     })
